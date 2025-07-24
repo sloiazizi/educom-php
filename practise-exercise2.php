9b86f5e5daf8ec2dded899514a741d57b5597f36
Sarah Loiazizi <sarah.loiazizi@hotmail.com>
2025-07-24 11:36:41 +0200

    PHP Comments practice + W3 exercises guessed correctly (#1)

---
+++ practise-exercise2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+
+    <?php
+    // commentje hier en daar 
+    /* oke nu heb ik er meerdere hehehe
+    achjajksdhjljdfhlakjhdsf
+    damn dit is best chill. ofja rene houdt niet van comments. hmmm. 
+    */    
+    # hashtags werken ook gewoon. waarom meerdere gebruiken? achja NIET DENKEN maar regels volgens so lets go i guess.
+    ?>
+
+    <?php
+    // Outputs a welcome message:
+    echo "Welcome Home!";
+
+    echo "Welcome Home!"; // Outputs a welcome message
+
+    ?>
+
+    </body>
+
+</html>
