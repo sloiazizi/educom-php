5fff71c57ae351ae2ebfdfb7b3c41649c83d1802
Sarah Loiazizi <sarah.loiazizi@hotmail.com>
2025-07-24 12:32:38 +0200

    PHP variable3 (#1)

--- practise-exercise3.php
+++ practise-exercise3.php
@@ -6,5 +6,13 @@
         echo "I am following a traineeship at " . $text . "!";
 
         ?>
+
+        <?php
+        $nummer = 5;      // $x is an integer
+        $naam = "John"; // $y is a string
+        echo $nummer;
+        echo $naam;
+
+        ?>
     </body>
 </html>
